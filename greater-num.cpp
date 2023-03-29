bac2f974f3f554d0dbc2251d3e31826c63e16f41
Codemyhobby <alhassandamilola16@gmail.com>
2023-03-29 02:02:57 +0100

    update

---
+++ greater-num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    if (num1 > num2) {
+        cout << "Larger number is " << num1 << endl;
+    } else {
+        cout << "Larger number is " << num2 << endl;
+    }
+
+    return 0;
+}
---
+++ loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter an integer value: ";
+    cin >> num;
+
+    for (int i = 0; i < num; i++) {
+        cout << "Welldone" << endl;
+    }
+
+    return 0;
+}
